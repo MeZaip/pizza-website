25374842f8f6901c82f55df755e86bfcf7ede5dc
MeZaip <mezaip2000@gmail.com>
2022-11-05 22:08:35 +0200

    improvement

--- functions.js
+++ functions.js
@@ -1,4 +1,19 @@
-const modal=document.getElementById('product-group')
-modal.addEventListener('click', function(){
-    document.getElementById('modal').style.display="block";
+const product=document.getElementById('addtocartbtn')
+const modal=document.getElementById('modal')
+product.addEventListener('click', function(){
+    modal.style.display="block";
 })
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+}
+window.onscroll=function(){scrollFunction()}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      document.getElementById("header").style.top = "0";
+    } else {
+      document.getElementById("header").style.top = "-100px";
+    }
+  }
