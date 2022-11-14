60f8e0604d22460467e5e78e4ea7a32f1d949744
MeZaip <mezaip2000@gmail.com>
2022-11-14 22:31:11 +0200

    meniunavbaradded

--- functions.js
+++ functions.js
@@ -11,9 +11,45 @@ window.onclick = function(event){
 window.onscroll=function(){scrollFunction()}
 
 function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-      document.getElementById("header").style.top = "0";
-    } else {
-      document.getElementById("header").style.top = "-100px";
-    }
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-100px";
   }
+}
+const pizzameniu=document.getElementById('pizzameniu')
+const pizzabutton=document.getElementById('pizza')
+const ofertemeniu=document.getElementById('ofertemeniu')
+const ofertebutton=document.getElementById('oferte')
+const antreurimeniu=document.getElementById('antreurimeniu')
+const antreuributton=document.getElementById('antreuri')
+const pastemeniu=document.getElementById('pastemeniu')
+const pastebutton=document.getElementById('paste')
+const bauturimeniu=document.getElementById('bauturimeniu')
+const bautuributton=document.getElementById('bauturi')
+let current=document.getElementById('pizzameniu')
+pizzabutton.onclick=function() {
+  current.style.display="none";
+  pizzameniu.style.display="block";
+  current=pizzameniu;
+}
+ofertebutton.onclick=function(){
+  current.style.display="none";
+  ofertemeniu.style.display="block";
+  current=ofertemeniu;
+}
+antreuributton.onclick=function() {
+  current.style.display="none";
+  antreurimeniu.style.display="block";
+  current=antreurimeniu;
+}
+pastebutton.onclick=function(){
+  current.style.display="none";
+  pastemeniu.style.display="block";
+  current=pastemeniu;
+}
+bautuributton.onclick=function(){
+  current.style.display="none";
+  bauturimeniu.style.display="block";
+  current=bauturimeniu;
+}
